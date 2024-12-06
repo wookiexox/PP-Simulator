e7dee68f5b3a710515666f67fef223f2ace3634a
wookiexox <lukaszwator26@gmail.com>
2024-12-06 01:53:43 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+internal class Animals
+{
+    public required string Description { get; set; }
+    public uint Size { get; set; } = 3;
+
+    public string Info
+    {
+        get { return $"{Description} <{Size}>"; }
+    }
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+internal class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature()
+    {
+
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+}
